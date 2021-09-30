751a1c4eb8fccda3570df17ffb635f69f6603537
Fernando Konflanz <konflanz.info@gmail.com>
2021-09-29 21:13:01 -0300

    Update 29092021.1
    + Implementando métodos para comunicação

---
+++ fernando_subscriber.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mosquitto.h>
+
+void on_connect(struct mosquitto *mosq, void *obj, int rc)
+{
+
+    printf("ID: %d\n", *(int *)obj);
+
+    if (rc)
+    {
+        printf("Erro codigo: %d\n", rc);
+        exit(-1);
+    }
+
+    mosquitto_subscribe(mosq, NULL, "msg-fernando/t1", 0);
+}
+
+void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg)
+{
+    printf("Nova mensagem de Patrick: %s: %s\n", msg->topic, (char *)msg->payload);
+}
+
+int main()
+{
+    int rc, id = 11;
+
+    struct mosquitto *mosq;
+
+    mosq = mosquitto_new("subscriber-fernando", true, &id);
+    mosquitto_connect_callback_set(mosq, on_connect);
+    mosquitto_message_callback_set(mosq, on_message);
+
+    rc = mosquitto_connect(mosq, "broker.emqx.io", 1883, 10);
+
+    if (rc)
+    {
+        printf("Nao foi possivel se conectar com o broker. Erro: %d\n", rc);
+        return -1;
+    }
+
+    mosquitto_loop_start(mosq);
+    printf("Pressione Enter para sair...\n");
+    getchar();
+    mosquitto_loop_stop(mosq, true);
+
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
+
+    return 0;
+}
---
+++ patrick_publisher.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mosquitto.h>
+
+int main()
+{
+
+    int rc;
+    struct mosquitto *mosq;
+    char user_name[100];
+    char user_message[500];
+    char destino[50];
+
+    mosquitto_lib_init();
+
+    mosq = mosquitto_new("publisher-patrick", true, NULL);
+
+    rc = mosquitto_connect(mosq, "broker.emqx.io", 1883, 60);
+
+    if (rc != 0)
+    {
+        printf("Client nao pode se conectar ao broker. Erro codigo: %d\n", rc);
+        mosquitto_destroy(mosq);
+        exit(-1);
+    }
+
+    printf("conexao estabelecida com sucesso! \n");
+
+    printf("Destino: ");
+    scanf("%[^\n]", destino);
+
+    printf("Digite sua mensagem: ");
+    scanf("%[^\n]", user_message);
+
+    if (destino == "fernando")
+    {
+        mosquitto_publish(mosq, NULL, "msg-fernando/t1", 500, user_message, 0, false); //msg-patrick/t1
+    }
+
+    if (destino == "grupo")
+    {
+        mosquitto_publish(mosq, NULL, "msg-grupo/t1", 500, user_message, 0, false);
+    }
+
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
+
+    return 0;
+}
